5b27245b37833283e60fe4845a0be10fae206aff
Danver Vernetti <120321677+RagZark@users.noreply.github.com>
2023-11-10 11:02:59 -0300

    Update buttons.js

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -24,17 +24,3 @@ document.querySelectorAll('.btn-price').forEach((element, id) => {
         window.open(url, '_blank')
     });
 });
-
-// let btnPrice = document.querySelectorAll('.btn-price')
-
-// btnPrice.forEach((element, id) => {
-//     element.addEventListener('click', () => {
-//         if(id === 0){
-//             window.open('https://api.whatsapp.com/send?phone=5553999572334&text=Oi%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20plano%20de%20UI!', '_blank')
-//         } else if(id === 1){
-//             window.open('https://api.whatsapp.com/send?phone=5553999572334&text=Oi%2C%20gostaria%20de%20saber%20mais%20sobre%20a%20pesquisa%20UX%2FUI!', '_blank')
-//         } else{
-//             window.open("https://api.whatsapp.com/send?phone=5553999572334&text=Oi%2C%20gostaria%20de%20saber%20mais%20sobre%20o%20plano%20de%20Identidade%20Visual!", '_blank')
-//         }
-//     });
-// });
